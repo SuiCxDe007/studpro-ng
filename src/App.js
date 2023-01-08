23615387a53dbbcabccfd7c0043d821a4a0d348e
Kaveen Abeyrathne <43596628+SuiCxDe007@users.noreply.github.com>
2023-01-08 13:11:25 +0530

    added scroll to top

--- src/App.js
+++ src/App.js
@@ -22,43 +22,33 @@ import SignIn from "./components/Admin/Auth/SignIn-component";
 import {Route, Routes} from 'react-router-dom';
 import ErrorPage from "./pages/Error-page";
 import DashboardPage from "./components/Admin/Pages/Dashbpard-page";
-import AuthContext from "./components/store/auth-context";
-import {useContext} from "react";
 import {useSelector} from "react-redux";
 import CarouselSettingsPage from "./components/Admin/Pages/CarouselSettings-page";
 import GalleryPage from "./pages/Gallery-Page";
 import GallerySettingsPage from "./components/Admin/Pages/GallerySettings-page";
+import ScrollToTop from "react-scroll-to-top";
 
 function App() {
-    //TODO Move to REDUX
-    const AuthCtx = useContext(AuthContext);
+
     const isAuth = useSelector(state => state.isAuthenticated)
     return (
 
-
         <div className="App">
-
+            <ScrollToTop smooth color="#FFA500"/>
             <Routes>
-
-
+                <Route path="/login" element={<SignIn/>}/>
                 <Route path="/" element={<LandingPage/>}/>
-
                 <Route path="/sessions" element={<SessionsPage/>}/>
                 <Route path="/gallery" element={<GalleryPage/>}/>
+                <Route path={"*"} element={<ErrorPage/>}/>
 
                 <Route path={"/dashboard"} element={isAuth ? <DashboardPage/> : <SignIn/>}/>
                 <Route path={"/session-settings"} element={isAuth ? <SessionSettingsPage/> : <SignIn/>}/>
                 <Route path={"/sponsor-settings"} element={isAuth ? <SponsorSettingsPage/> : <SignIn/>}/>
                 <Route path={"/carousel-settings"} element={isAuth ? <CarouselSettingsPage/> : <SignIn/>}/>
                 <Route path={"/gallery-settings"} element={isAuth ? <GallerySettingsPage/> : <SignIn/>}/>
-
-                <Route path="/login" element={<SignIn/>}/>
-                <Route path={"*"} element={<ErrorPage/>}/>
-
             </Routes>
-
         </div>
-
     );
 }
 
